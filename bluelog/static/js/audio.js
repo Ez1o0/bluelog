ecc929b4a398372fcda50ccc8e6aea4a1be3f9d4
Ez1o0 <49774511+Ez1o0@users.noreply.github.com>
2020-07-11 09:55:39 +0800

    audioplayer final-ver

---
+++ bluelog/static/js/audio.js
@@ -0,0 +1,60 @@
+function setCookie(c_name,value,exdays)
+{
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + 
+    ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value+ "; path=/";
+}
+
+function getCookie(c_name)
+{
+    var i,x,y,ARRcookies=document.cookie.split(";");
+    for (i=0;i<ARRcookies.length;i++)
+    {
+      x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+      y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+      x=x.replace(/^\s+|\s+$/g,"");
+      if (x==c_name)
+        {
+        return unescape(y);
+        }
+      }
+}
+
+/* function checkCookie(c_name) {     
+    username = getCookie(c_name);     
+    console.log(username);     
+    if (username != null && username != "")     
+    { return true; }     
+    else     
+    { return false;  }
+}
+
+function clearCookie(name) {     
+    setCookie(name, "", -1); 
+} */
+
+var song = document.getElementsByTagName('audio')[0];
+var played = false;
+var tillPlayed = getCookie('timePlayed');
+function update()
+{
+    if(!played){
+        if(tillPlayed){
+        song.currentTime = tillPlayed;
+        song.play();
+        played = true;
+        }
+        else {
+                song.play();
+                played = true;
+        }
+    }
+
+    else {
+    setCookie('timePlayed', song.currentTime);       
+    }
+}
+setInterval(update,1000); 
+
