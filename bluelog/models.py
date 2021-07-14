a0fa215411983766a68123a1798cc3a7d8c46621
ez1o0 <411152084@qq.com>
2021-07-14 23:17:14 +0800

    Update models.py

--- bluelog/models.py
+++ bluelog/models.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-    :author: Grey Li (李辉)
-    :url: http://greyli.com
-    :copyright: © 2018 Grey Li <withlihui@gmail.com>
-    :license: MIT, see LICENSE for more details.
+    数据库的字段
 """
 from datetime import datetime
 
